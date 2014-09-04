0efa931e412a098d7372f17e4deb741d12c2f369
Luis Vega <luis@glamdring.local>
2014-09-04 22:40:03 +0800

    Use switch statement

--- body.go
+++ body.go
@@ -7,38 +7,36 @@ import (
 
 func Parse(params map[string][]string, s interface{}) (err error) {
 	v := reflect.ValueOf(s).Elem()
+	t := v.Type()
 
 	for i := 0; i < v.NumField(); i++ {
-		field_value := v.Field(i)
-		field := v.Type().Field(i)
-		tagName := field.Tag.Get("name")
+		var (
+			field     = v.Field(i)
+			tagName   = t.Field(i).Tag.Get("name")
+			param_val = params[tagName]
+		)
 
-		param_val := params[tagName]
-
-		if field_value.Kind() == reflect.String {
-			field_value.SetString(param_val[0])
-		}
-
-		if field_value.Kind() == reflect.Int {
+		switch field.Kind() {
+		case reflect.String:
+			field.SetString(param_val[0])
+		case reflect.Int:
 			integer, _ := strconv.ParseInt(param_val[0], 10, 64)
-			field_value.SetInt(integer)
-		}
-
-		if field_value.Kind() == reflect.Slice {
-			slice := reflect.MakeSlice(field_value.Type(), 0, 0)
+			field.SetInt(integer)
+		case reflect.Slice:
+			slice := reflect.MakeSlice(field.Type(), 0, 0)
 
 			for _, s := range param_val {
-				if field_value.Type().Elem().Kind() == reflect.String {
+				if field.Type().Elem().Kind() == reflect.String {
 					slice = reflect.Append(slice, reflect.ValueOf(s))
 				}
 
-				if field_value.Type().Elem().Kind() == reflect.Int64 {
+				if field.Type().Elem().Kind() == reflect.Int64 {
 					integer, _ := strconv.ParseInt(s, 10, 64)
 					slice = reflect.Append(slice, reflect.ValueOf(integer))
 				}
 			}
 
-			field_value.Set(slice)
+			field.Set(slice)
 		}
 	}
 
