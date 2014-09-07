22260db7c421cd55c2f552d35582e5aebc650f4f
Luis Vega <luis@glamdring.local>
2014-09-08 00:12:25 +0800

    Refactor

--- body.go
+++ body.go
@@ -1,48 +1,36 @@
 package body
 
 import (
-	"fmt"
 	"reflect"
 	"strconv"
 )
 
 func Parse(params map[string][]string, s interface{}) (err error) {
-	parser := Parser{
-		params: params,
-		struc:  s,
-		val:    reflect.ValueOf(s).Elem(),
-		typ:    reflect.ValueOf(s).Elem().Type()}
+	val := reflect.ValueOf(s).Elem()
+	typ := reflect.ValueOf(s).Elem().Type()
 
-	err = parser.parse()
-
-	return
-}
-
-type Parser struct {
-	params map[string][]string
-	struc  interface{}
-	val    reflect.Value
-	typ    reflect.Type
-}
-
-func (p Parser) parse() (err error) {
-	for i := 0; i < p.val.NumField(); i++ {
+	for i := 0; i < val.NumField(); i++ {
 		var (
-			field     = p.val.Field(i)
-			tagName   = p.typ.Field(i).Tag.Get("name")
-			param_val = p.params[tagName]
+			field    = val.Field(i)
+			tagName  = typ.Field(i).Tag.Get("name")
+			paramVal []string
+			match    bool
 		)
+
+		if paramVal, match = params[tagName]; match == false {
+			continue
+		}
 
 		switch field.Kind() {
 		case reflect.String:
-			field.SetString(param_val[0])
+			field.SetString(paramVal[0])
 		case reflect.Int:
-			integer, _ := strconv.ParseInt(param_val[0], 10, 64)
+			integer, _ := strconv.ParseInt(paramVal[0], 10, 64)
 			field.SetInt(integer)
 		case reflect.Slice:
 			slice := reflect.MakeSlice(field.Type(), 0, 0)
 
-			for _, item := range param_val {
+			for _, item := range paramVal {
 				switch field.Type().Elem().Kind() {
 				case reflect.String:
 					slice = reflect.Append(slice, reflect.ValueOf(item))
